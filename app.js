1d157f33987eb3df5e2c599914385769236f97a9
Cameron Kilgore <ghostfreeman@gmail.com>
2022-08-10 14:16:37 -0500

    Added CORS

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const express = require('express')
-const app = express()
+const cors = require('cors')
 const router = express.Router()
 const database = require('./database')
 const Greeting = require('./models/greeting')
-const cors = require('cors')
 
 const port = 8080
 
+const app = express()
 app.use(express.json())
-app.use(cors({
-    origin: '*'
-}))
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.json({'greeting': 'Hello!'})
