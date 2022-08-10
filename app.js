2a08d781781f7e49b88f0387bb4896973b9c4f41
Cameron Kilgore <ghostfreeman@gmail.com>
2022-08-10 11:50:16 -0500

    Added POST and DELETE action

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const port = 8080
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    // TODO Send random greeting
     res.json({'greeting': 'Hello Olleh!'})
 })
 
@@ -19,40 +18,49 @@ app.get('/healthcheck', (req, res) => {
 
 app.get('/greeting', (req, res) => {
     // TODO Get random salutation
+
+    // TODO get a salutation that starts the same date as the server is
 })
 
-app.get('/greeting/all', (req, res) => {
-    // TODO Get all salutations
-})
+app.get('/greeting/all', async (req, res) => {
+    const greetings = await Greeting.find({});
 
-app.post('/greeting/create', (req, res) => {
-    const data = req.body
-    // TODO There has to be a way to do this with spread/rest
-    salutation = {
-        text: data.text,
-        start_date: data.start_date,
-        end_date: data.end_date,
-        clamp_to_morning: data.clamp_to_morning,
-        clamp_to_afternoon: data.clamp_to_afternoon,
-        clamp_to_evening: data.clamp_to_evening,
-        likelihood: data.likelihood,
+    try {
+        res.send(greetings)
+    } catch (error) {
+        res.status(500).send(error)
     }
-
-    const greeting = new Greeting(data)
-    greeting.save(function (err) {
-        console.log("Error in saving record", err)
-        res.status(err.status || 500);
-    })
-
-    res.json(salutation)
 })
 
-app.put('/greeting/NUM', (req, res) => {
+app.post('/greeting/create', async (req, res) => {
+    const greeting = new Greeting(req.body)
+
+    try {
+        await greeting.save()
+        res.send(greeting)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+app.put('/greeting/:id', (req, res) => {
     // TODO Update Salutation
 })
 
-app.delete("/greeting/NUM", (req, res) => {
-    // TODO Delete Salutation
+app.delete("/greeting/:id", (req, res) => {
+    if (!req.params.id) {
+        res.status(400).send()
+    }
+
+    const greeting = Greeting.deleteOne({id: req.params.id}, function(err, data) {
+        if (!err) {
+            console.log("Greeting " + req.params.id + " deleted")
+            res.status(200).send()
+        } else {
+            console.log({error})
+            res.status(500).send()
+        }
+    })
 })
 
 app.listen(port, function () {
